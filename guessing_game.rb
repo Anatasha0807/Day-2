2d9a4ee380ae90e5f1714c1e73d16f8989365b16
Anatasha <anatashadyahia.88@gmail.com>
2019-08-01 17:28:13 +0800

    Final Guessing Game

--- guessing_game.rb
+++ guessing_game.rb
@@ -2,79 +2,113 @@
 old_sync = $stdout.sync
 $stdout.sync = true
 
+@play = true
+
+# set initial values
+@easy_secret = rand(1..10)
+@medium_secret = rand(1..50)
+@hard_secret = rand(1..100)
+@history = {}
+@previous_tries = []
+
 puts "Welcome to Guessing Game"
-puts "What's your name?"
-name = gets.chomp
-puts "Hi #{name}"
 
-easy_secret = rand(1..10)
-medium_secret = rand(1..50)
-hard_secret = rand(1..100)
-
-history = {}
-
-puts "Please Choose Your Level Difficulty?"
-puts "1 - Easy"
-puts "2 - Medium"
-puts "3 - Hard"
-
-choice = gets.to_i
-
-puts "You Choose Level #{choice}"
-
-if choice == 1
-  secret_number = easy_secret
-  maximum_number = 10
-  chances = 6
-elsif choice == 2
-  secret_number = medium_secret
-  maximum_number = 50
-  chances = 4
-elsif choice == 3
-  secret_number = hard_secret
-  maximum_number = 100
-  chances = 2
-else
-  exit
+def ask_for_name
+  puts "What's your name?"
+  @name = gets.chomp
+  return @name
 end
 
-puts "Pick any number from 1 to #{maximum_number}?"
-puts "Secret: #{secret_number}"
-guess = gets.chomp.to_i
+def select_difficulty_level
+  puts "Please Choose Your Level Difficulty?"
+  puts "1 - Easy"
+  puts "2 - Medium"
+  puts "3 - Hard"
 
-count = 1
-previous_tries = []
+  @choice = gets.to_i
+  return @choice
+end
 
-while count < chances
-  if secret_number == guess
-    puts "You Are Awesome!"
-    history[name] = chances - 1
-
-    puts "Leaderboard"
-    puts "==========="
-
-    puts history
-
-
-      puts "Do You Want To Play Again?"
-      puts "Y - Yes and Play Again"
-      puts "N - No and Exit"
-
-    break
+def set_default_values
+  if @choice == 1
+    @secret_number = rand(1..10)
+    @maximum_number = 10
+    @chances = 6
+  elsif @choice == 2
+    @secret_number = rand(1..50)
+    @maximum_number = 50
+    @chances = 4
+  elsif @choice == 3
+    @secret_number = rand(1..100)
+    @maximum_number = 100
+    @chances = 2
   else
-    puts "Try Again"
-    previous_tries << guess
-    puts "#{previous_tries}"
-    count += 1
-    guess = gets.to_i
+    exit
   end
+  return @secret_number, @maximum_number, @chances
+end
 
-  if secret_number != guess
-    puts "You Lose"
-    if count == chances
-      puts "Game Over"
-      puts "the correct answer: #{secret_number}"
-      exit
-    end
+def game_begins
+  puts "Pick any number from 1 to #{@maximum_number}?"
+  guess = gets.chomp.to_i
+
+  count = 1
+
+  while count < @chances
+   if @secret_number == guess
+     puts "You Are Awesome!"
+
+     @history[@name] = @chances - 1
+
+     puts "Leaderboard"
+     puts "==========="
+
+     puts @history
+
+     prompt_play
+
+     # break
+   else
+     puts "Try Again"
+     @previous_tries << guess
+     puts "#{@previous_tries}"
+     count += 1
+     guess = gets.to_i
+   end
+
+   if @secret_number != guess
+     puts "You Lose"
+     if count == @chances
+       puts "Game Over"
+       puts "the correct answer: #{@secret_number}"
+       exit
+     end
+   end
   end
 end
+
+def prompt_play
+  puts "Do You Want To Play Again?"
+  puts "Y - Yes and Play Again"
+  puts "N - No and Exit"
+
+  answer = gets.chomp
+  if answer == 'y'
+    run_game
+  else
+    exit
+  end
+end
+
+def run_game
+  loop do
+    ask_for_name
+    puts "Hi #{@name}"
+    select_difficulty_level
+    puts "You Choose Level #{@choice}"
+    set_default_values
+    game_begins
+  end
+end
+
+run_game
